6efae058dd2fd8887766efdda4c432d3701f5347
Anastasiya Khinevich <xinevich.av@gmail.com>
2022-11-06 17:27:17 +0100

    add method CalculatorBroca with Exception

--- src/main/java/by/itacademy/team2/Calculator.java
+++ src/main/java/by/itacademy/team2/Calculator.java
@@ -1,34 +1,23 @@
 package by.itacademy.team2;
 
+import org.checkerframework.checker.units.qual.C;
+
 public class Calculator {
-    float height;
-    float weight;
 
-    public Calculator() {
-    }
+    public int calculateIdealWeightByBroca(int height) {
 
-    public Calculator(float height) {
-        this.height = height;
-    }
-
-    public Calculator(float height, float weight) {
-        this.height = height;
-        this.weight = weight;
-    }
-
-    public float getHeight() {
-        return height;
-    }
-
-    public void setHeight(float height) {
-        this.height = height;
-    }
-
-    public float getWeight() {
-        return weight;
-    }
-
-    public void setWeight(float weight) {
-        this.weight = weight;
+        if (height < 100) {
+            throw new IllegalArgumentException("If height < 100 weight will be negative. Please enter the correct height!");
+        }
+        int ratioBroca = 0;
+        if (height < 165) {
+            ratioBroca = 100;
+        } else if (height < 175) {
+            ratioBroca = 105;
+        } else {
+            ratioBroca = 110;
+        }
+        int idealWeightByBroca = height - ratioBroca;
+        return idealWeightByBroca;
     }
 }
