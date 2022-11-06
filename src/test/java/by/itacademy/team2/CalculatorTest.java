897becb7345825d9e0ed7fe25568cb08a293ec9a
Anastasiya Khinevich <xinevich.av@gmail.com>
2022-11-06 20:17:33 +0100

    refactor class CalculatorTest

---
+++ src/test/java/by/itacademy/team2/CalculatorTest.java
@@ -0,0 +1,33 @@
+package by.itacademy.team2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    static Calculator calculator;
+
+    @BeforeClass
+    public static void beforeTest(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testCalculateIdealWeightByBroca(){
+        //CalculatorBroca calculator = new CalculatorBroca();
+        //calculator.calculate();
+        //assertEquals(6, sum)
+    }
+
+    @Test
+    public void testCalculateIdealWeightByLorentz(){
+        float height = 200f;
+        float expected = 25f;
+
+//        float actual = calculator.calculateIdealWeightByLorentz();
+//
+//        Assert.assertEquals(expected,actual,0.1);
+    }
+}
