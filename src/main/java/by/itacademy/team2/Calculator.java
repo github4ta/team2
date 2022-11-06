f81f15da8cd9261541265ad7926f30c4b6daad3a
aleksei <6001mx7@gmail.com>
2022-11-06 15:42:59 +0300

    refactor Calculator

--- src/main/java/by/itacademy/team2/Calculator.java
+++ src/main/java/by/itacademy/team2/Calculator.java
@@ -1,34 +1,4 @@
 package by.itacademy.team2;
 
 public class Calculator {
-    float height;
-    float weight;
-
-    public Calculator() {
-    }
-
-    public Calculator(float height) {
-        this.height = height;
-    }
-
-    public Calculator(float height, float weight) {
-        this.height = height;
-        this.weight = weight;
-    }
-
-    public float getHeight() {
-        return height;
-    }
-
-    public void setHeight(float height) {
-        this.height = height;
-    }
-
-    public float getWeight() {
-        return weight;
-    }
-
-    public void setWeight(float weight) {
-        this.weight = weight;
-    }
 }
