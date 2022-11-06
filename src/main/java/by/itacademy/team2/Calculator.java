833d38a76f7add8c48febd15881c5eaa2eec9942
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-11-06 14:20:51 +0100

    refactor calculateIdealWeightByLorentz method

--- src/main/java/by/itacademy/team2/Calculator.java
+++ src/main/java/by/itacademy/team2/Calculator.java
@@ -2,33 +2,12 @@ package by.itacademy.team2;
 
 public class Calculator {
     float height;
-    float weight;
 
-    public Calculator() {
-    }
-
-    public Calculator(float height) {
-        this.height = height;
-    }
-
-    public Calculator(float height, float weight) {
-        this.height = height;
-        this.weight = weight;
-    }
-
-    public float getHeight() {
-        return height;
-    }
-
-    public void setHeight(float height) {
-        this.height = height;
-    }
-
-    public float getWeight() {
-        return weight;
-    }
-
-    public void setWeight(float weight) {
-        this.weight = weight;
+    public int calculateIdealWeightByLorentz() {
+        if (height != 0) {
+            float weightResult = (height - 100) - (height - 150) / 2;
+            return (int) weightResult;
+        }
+        return 0;
     }
 }
