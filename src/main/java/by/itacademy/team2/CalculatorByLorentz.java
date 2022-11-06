199760f9d96168c4cb8c7e4027070fb2f0e344b0
Aliaksandr Viartseika <aliaksandr_viartseika@leverx.com>
2022-11-06 11:53:56 +0100

    add calculateIdealWeightByLorentz method

--- src/main/java/by/itacademy/team2/CalculatorByLorentz.java
+++ src/main/java/by/itacademy/team2/CalculatorByLorentz.java
@@ -1,5 +1,16 @@
 package by.itacademy.team2;
 
-public class CalculatorByLorentz extends Calculator{
+public class CalculatorByLorentz extends Calculator {
 
+    public CalculatorByLorentz(float height) {
+        super(height);
+    }
+
+    public int calculateIdealWeightByLorentz() {
+        if (getHeight() != 0) {
+            float weightResult = (getHeight() - 100) - (getHeight() - 150) / 2;
+            return (int) weightResult;
+        }
+        return 0;
+    }
 }
