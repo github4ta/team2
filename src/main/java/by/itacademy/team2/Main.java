9c33508056d54add0405850795e8d3346aebd68e
aleksei <6001mx7@gmail.com>
2022-11-06 13:40:12 +0300

    add CalculatorNegler

---
+++ src/main/java/by/itacademy/team2/Main.java
@@ -0,0 +1,8 @@
+package by.itacademy.team2;
+
+public class Main {
+    public static void main(String[] args) {
+        CalculatorNegler CalculatorNeglerObj = new CalculatorNegler(170);
+        System.out.println(Math.round(CalculatorNeglerObj.idealWeight()));
+    }
+}
