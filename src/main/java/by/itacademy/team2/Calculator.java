70378f6dc6e3c94195d6e5ce0bac3f431b2808a0
aleksei <6001mx7@gmail.com>
2022-11-06 19:48:43 +0300

    refactor Calculator

--- src/main/java/by/itacademy/team2/Calculator.java
+++ src/main/java/by/itacademy/team2/Calculator.java
@@ -1,4 +1,23 @@
 package by.itacademy.team2;
 
 public class Calculator {
+    float height;
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float сalculateIdealWeightByNegler(){
+        float idealWeight = 45;
+        float currentHeight = getHeight();
+        currentHeight -= 152.4f;
+        idealWeight += 0.9f * (currentHeight / 2.45f);
+        idealWeight *= 1.1f;
+
+        return idealWeight;
+    }
 }
